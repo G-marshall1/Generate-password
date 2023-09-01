696909cfb2353bb540fe750ad446ec91524dbec6
Graham Marshall <grahammarshall@Grahams-MacBook-Air.local>
2023-08-31 21:48:32 -0600

    Added charaters into global scope

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,14 @@
 // Assignment Code
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var specialCharater = "?/!@#$%^&*()_+=\|][';:.,<>`~{}-";
+var password = "";
+
+
+
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
