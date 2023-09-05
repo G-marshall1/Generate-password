af06516440f777e907c6f2864acf3943cfca5ecd
Graham Marshall <grahammarshall@Grahams-MacBook-Air.local>
2023-09-05 13:42:33 -0600

    Added comments to create steps, creating condional statements, functions, and object. Also forgot to push before I did most of this...

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,65 @@
-// defined all possible charaters in the global scope for a password
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbers = "0123456789";
-var specialCharater = "?/!@#$%^&*()_+=\|][';:.,<>`~{}-";
-var password = "";
+//arrays in global
 
+//4 functions needed
+//function 1 collects users choices
+//function 2 is give random choice
+// function 3 generate password
+//function 4 is write password
 
 
 var generateBtn = document.querySelector("#generate");
+var lowerCase = []
+var upperCase = [""]
+var specialCharater = [""]
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+function options() {
+  //give options 
+  var passwordLength = parseInt(prompt("How many charaters do you want to have? Pick a number betwen 8-128."))
+
+  if (isNaN(passwordLength) === true) {
+alert("You must pick a number!")
+return;
+  }
 
 
-//need to add a confirm prompt to ask user if they want to use lower case, upper case, number, and special charater.
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Password must be between 8-128 charaters")
+    return;
+  }
 
+  var useLowerCase = confirm("Do you want to uses lower case letters? Click 'Ok' to confirm or 'cancel' to reject.")
+  var useUpperCase = confirm("Do you want to uses upper case letters? Click 'Ok' to confirm or 'cancel' to reject.")
+  var useSpecialCharater = confirm("Do you want to uses special charaters? Click 'Ok' to confirm or 'cancel' to reject.")
+  var useNumbers = confirm("Do you want to uses numbers? Click 'Ok' to confirm or 'cancel' to reject.")
+
+  //create a conditional statements that checks if they said no to everything
+
+
+  //then create object to store the users input
+var usersChoice = {
+  passwordLength: passwordLength,
+  useLowerCase: useLowerCase,
+  useUpperCase: useUpperCase,
+  useNumbers: useNumbers,
+  useSpecialCharater:useSpecialCharater
+}
+
+  //retrun object
+return usersChoice
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+  // passwordText.value = password;
+options()
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
