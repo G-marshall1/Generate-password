b7a6b0779b10ee652e1320b95a37f5608ef66819
Graham Marshall <grahammarshall@Grahams-MacBook-Air.local>
2023-09-05 20:59:17 -0600

    Added conditional if they didn't pick any charaters

--- Develop/script.js
+++ Develop/script.js
@@ -8,9 +8,9 @@
 
 
 var generateBtn = document.querySelector("#generate");
-var lowerCase = []
-var upperCase = [""]
-var specialCharater = [""]
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k" , "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K" , "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var specialCharater = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "{", "}", "[", "]", "|", "/", "<", ">", "?"]
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 function options() {
@@ -34,7 +34,10 @@ return;
   var useNumbers = confirm("Do you want to uses numbers? Click 'Ok' to confirm or 'cancel' to reject.")
 
   //create a conditional statements that checks if they said no to everything
-
+if (useLowerCase === false && useUpperCase === false && useSpecialCharater === false && useNumbers === false) {
+  alert("You must select one.")
+  return;
+}
 
   //then create object to store the users input
 var usersChoice = {
